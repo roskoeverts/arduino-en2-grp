e789ab57a74e1a94173d19d4279d1ac757ad8569
roskoeverts <roskoeverts@gmail.com>
2023-04-05 12:56:35 -0500

    robert test push

---
+++ final-project-en2.ino
@@ -0,0 +1,32 @@
+void setup() {
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_D1  37
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_G1  49
+#define NOTE_A1  55
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_D2  73
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_G2  98
+#define NOTE_A2  110
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_D3  147
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_G3  196
+#define NOTE_A3  220
+#define NOTE_B3  247
+#define REST 0
+
+int tempo = 175
+int buzzer = 13
+
+NOTE_E2,NOTE_E2,NOTE_E2,NOTE_E2
+NOTE_D2,NOTE_C2,NOTE_D2,NOTE_E2
+NOTE_E2,
+}
