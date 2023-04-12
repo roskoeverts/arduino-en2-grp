76a5230938f7ad98b4d7c306c0ed15a5152133a0
roskoeverts <roskoeverts@gmail.com>
2023-04-12 12:42:56 -0500

    idek

---
+++ final-project-en2/final-project-en2.ino
@@ -0,0 +1,44 @@
+#define B0  31
+#define C1  33
+#define D1  37
+#define E1  41
+#define F1  44
+#define G1  49
+#define A1  55
+#define B1  62
+#define C2;  65;
+#define D2;  73;
+#define E2;  82
+#define F2  87
+#define G2  98
+#define A2  110
+#define B2  123
+#define C3  131
+#define D3  147
+#define E3  165
+#define F3  175
+#define G3  196
+#define A3  220
+#define B3  247
+#define REST 0;
+
+int tempo = 343;
+int buzzer = 13;
+int dotquarter = tempo/2;
+int quarter = tempo/3;
+int sixteenth = tempo/6;
+int dothalf = tempo * (10/9);
+
+void setup() {
+for (int i = 0; i < 5; i++){
+  tone(output, E2);
+  
+}
+}
+
+void loop() {
+
+NOTE_E2 NOTE_E2 NOTE_E2 NOTE_E2;
+NOTE_D2 NOTE_C2 NOTE_D2 NOTE_E2;
+NOTE_E2;
+}
