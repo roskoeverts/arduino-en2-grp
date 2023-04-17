b867daffc4ac56b9a92b10d87103b1e198fa8466
roskoeverts <roskoeverts@gmail.com>
2023-04-17 13:14:43 -0500

    4-17-23

--- final-project-en2/final-project-en2.ino
+++ final-project-en2/final-project-en2.ino
@@ -6,9 +6,9 @@
 #define G1  49
 #define A1  55
 #define B1  62
-#define C2;  65;
-#define D2;  73;
-#define E2;  82
+#define C2  65
+#define D2  73
+#define E2  82
 #define F2  87
 #define G2  98
 #define A2  110
@@ -20,25 +20,80 @@
 #define G3  196
 #define A3  220
 #define B3  247
-#define REST 0;
+#define REST 0
 
-int tempo = 343;
+int tempo = 1371;
 int buzzer = 13;
 int dotquarter = tempo/2;
-int quarter = tempo/3;
+int quarter = tempo/4;
 int sixteenth = tempo/6;
 int dothalf = tempo * (10/9);
+int output = 13;
 
 void setup() {
 for (int i = 0; i < 5; i++){
   tone(output, E2);
+  delay(quarter);
+  noTone(output);
+  delay(10);
   
+  tone(output, E2);
+  delay(quarter);
+  noTone(output);
+  delay(100);
+
+  tone(output, E2);
+  delay(quarter);
+  noTone(output);
+  delay(100);
+
+  tone(output, E2);
+  delay(quarter);
+  noTone(output);
+  delay(100);
+  
+  tone(output, D2);
+  delay(quarter);
+  noTone(output);
+  delay(100);
+
+  tone(output, C2);
+  delay(quarter);
+  noTone(output);
+  delay(100);
+
+  tone(output, D2);
+  delay(quarter);
+  noTone(output);
+  delay(100);
+  
+  tone(output, E2);
+  delay(quarter);
+  noTone(output);
+  delay(100);
+
+  tone(output, E2);
+  delay(quarter);
+  noTone(output);
+  delay(100);
+
+  tone(output, C2);
+  delay(quarter);
+  noTone(output);
+  delay(100);
+
+  tone(output, E2);
+  delay(quarter);
+  noTone(output);
+  delay(100);
+
+  tone(output, E2);
+  delay(dothalf);
+  noTone(output);
+  delay(100);
 }
 }
 
 void loop() {
 
-NOTE_E2 NOTE_E2 NOTE_E2 NOTE_E2;
-NOTE_D2 NOTE_C2 NOTE_D2 NOTE_E2;
-NOTE_E2;
 }
